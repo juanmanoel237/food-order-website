3774e849a447b11147155702b17acaa63b579461
juanmanoel237 <ndeguemanoeljuan@gmail.com>
2022-12-21 18:46:23 +0100

    first commit

---
+++ admin/index.php
@@ -0,0 +1,64 @@
+<html>
+    <head>
+        <title>Food Order Website - Home page</title>
+
+        <link rel = "stylesheet" href ="../css/admin.css">
+    </head>
+
+    <body>
+        <!-- Debut du menu-->
+        <div class="menu text-center">
+            <div class="wrapper">
+              <ul>
+                 <li><a href="#">Home</a></li>
+                 <li><a href="#">Admin</a></li>
+                 <li><a href="#">Category</a></li>
+                 <li><a href="#">Food</a></li>
+                 <li><a href="#">Order</a></li>
+              </ul>
+            </div>
+        </div>
+        <!-- Fin du menu-->
+
+        <!-- Main-->
+        <div class = "main-content">
+            <div class = "wrapper">
+            <h1>DASHBOARD</h1>
+
+            <div class="col-4" text-center>
+                <h1>5</h1>
+                <br/>
+                Categories
+            </div>
+
+            <div class="col-4" text-center>
+                <h1>5</h1>
+                <br/>
+                Categories
+            </div>
+
+            <div class="col-4" text-center>
+                <h1>5</h1>
+                <br/>
+                Categories
+            </div>
+
+            <div class="col-4" text-center>
+                <h1>5</h1>
+                <br/>
+                Categories
+            </div>
+            <div class="clearfixe"></div>
+            </div>
+        </div>
+        <!--fin Main-->
+
+        <!-- Footer-->
+        <div class = "footer">
+            <div class = "wrapper">
+               <p class ="text-center">2022 All rights reserbed, Some Reserved Developed by <a href="https://www.snapchat.com/add/juan_manoel237"> Juan Manoel</a>
+            </div>
+        </div>
+        <!-- Fin Footer-->
+    </body>
+</html>
