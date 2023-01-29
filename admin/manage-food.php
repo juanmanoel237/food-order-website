ea84e9b73b9c2261bd8d6d45e4f3b3f908a083b3
juanmanoel237 <ndeguemanoeljuan@gmail.com>
2023-01-30 00:42:23 +0100

    ...

--- admin/manage-food.php
+++ admin/manage-food.php
@@ -3,51 +3,59 @@
 <div class="main-content">
     <div class="wrapper">
         <h1>Manage Food</h1>
-        
-        <br/><br/><br/>
-            <!---- BUTTON TO ADD ADMIN ---->
-            <a href="#" class="btn-primary">Add Food</a>
-             <!----END BUTTON TO ADD ADMIN ---->
-            <br/><br/><br/>
-            
-            <table class ="tbl-full">
-                <tr>
-                    <th>S.N</th>
-                    <th>Full Name</th>
-                    <th>Username</th>
-                    <th>Actions</th>
-                </tr>
 
-                <tr>
-                    <td>1.</td>
-                    <td>Juan Manoel</td>
-                    <td>juanmanoel</td>
-                    <td>
+        <br /><br /><br />
+
+        <!---- BUTTON TO ADD ADMIN ---->
+        <a href="<?php echo SITEURL ?>admin/add-food.php" class="btn-primary">Add Food</a>
+        <!----END BUTTON TO ADD ADMIN ---->
+        <br /><br /><br />
+
+        <?php
+        if (isset($_SESSION['add'])) {
+            echo $_SESSION['add'];
+            unset($_SESSION['add']);
+        }
+        ?>
+
+        <table class="tbl-full">
+            <tr>
+                <th>S.N</th>
+                <th>Full Name</th>
+                <th>Username</th>
+                <th>Actions</th>
+            </tr>
+
+            <tr>
+                <td>1.</td>
+                <td>Juan Manoel</td>
+                <td>juanmanoel</td>
+                <td>
                     <a href="#" class="btn-secondary">Update Admin</a>
-                        <a href="#" class="btn-danger">Delete Admin</a>
-                    </td>
-                </tr>
+                    <a href="#" class="btn-danger">Delete Admin</a>
+                </td>
+            </tr>
 
-                <tr>
-                    <td>2.</td>
-                    <td>Juan Manoel</td>
-                    <td>juanmanoel</td>
-                    <td>
-                        <a href="#" class="btn-secondary">Update Admin</a>
-                        <a href="#" class="btn-danger">Delete Admin</a>
-                    </td>
-                </tr>
-
-                <tr>
-                    <td>3.</td>
-                    <td>Juan Manoel</td>
-                    <td>juanmanoel</td>
-                    <td>
+            <tr>
+                <td>2.</td>
+                <td>Juan Manoel</td>
+                <td>juanmanoel</td>
+                <td>
                     <a href="#" class="btn-secondary">Update Admin</a>
-                        <a href="#" class="btn-danger">Delete Admin</a>
-                    </td>
-                </tr>
-            </table>
+                    <a href="#" class="btn-danger">Delete Admin</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>3.</td>
+                <td>Juan Manoel</td>
+                <td>juanmanoel</td>
+                <td>
+                    <a href="#" class="btn-secondary">Update Admin</a>
+                    <a href="#" class="btn-danger">Delete Admin</a>
+                </td>
+            </tr>
+        </table>
     </div>
 </div>
 
